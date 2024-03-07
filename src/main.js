a2dfd1153b2aa4ecea2f9181c88275f58ab44c4c
Hao Duong <haowdy26@gmail.com>
2024-03-07 04:30:45 -0500

    trying

--- src/main.js
+++ src/main.js
@@ -83,11 +83,38 @@ async function sendEmail(email, hash) {
     }
 }
 
-app.get('check-authenication', (request, response) => {
-    if (!request.session.isAuthenicated){
-        return res.status(200);
-    }
+// main.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Check authentication status when the DOM is loaded
+    checkAuthenticationStatus();
 });
+
+// function checkAuthenticationStatus() {
+//     // Make an AJAX request to the endpoint that checks authentication status
+//     fetch('/check-authentication')
+//         .then(response => {
+//             if (response.ok) {
+//                 // If the response is successful, parse the JSON data
+//                 return response.json();
+//             } else {
+//                 throw new Error('Failed to fetch authentication status');
+//             }
+//         })
+//         .then(data => {
+//             // If the user is authenticated, show the Leaflet map
+//             if (data.isAuthenticated) {
+//                 document.getElementById('wp2').style.display = 'block';
+//             } else {
+//                 // If the user is not authenticated, hide the Leaflet map
+//                 document.getElementById('wp2').style.display = 'none';
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error checking authentication status:', error);
+//         });
+// }
+
 app.get('/tiles/:l/:x/:y', (req, res) => {
     if (!req.session.isAuthenicated){
         return res.status(200).json({ status: "ERROR" })
